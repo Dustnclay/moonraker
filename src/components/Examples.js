939b3a968b8416de59a306d8b167554f3f316d8c
dustin <dustnclaytiner@gmail.com>
2021-01-08 21:32:59 -0600

    cache removed and reinstalled

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -12,6 +12,7 @@ const Examples = () => {
             <h1 class="title">Examples of our work</h1>
             {/* <h2>Our {season} line</h2> */}
             <div>
+                
                 <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
                 <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
                 <img class="image" src='MMC_Icon_Gold.jpg' alt='earrings'/>
